840e9b58c60399444d7e104ebbfba4575e405f8e
TimurTimergalin <timur20041229@gmail.com>
2024-02-17 18:19:41 +0300

    Алгоритм для построения префикса развертки произвольного размера

--- unfoldings/__init__.py
+++ unfoldings/__init__.py
@@ -1,8 +1,10 @@
 from .standard import build_prefix as standard_algorithm
+from .no_cutoffs import build_unfolding as unfolding_algorithm
 from .n_safe import build_prefix as n_safe_algorithm, NSafeSettings
 
 __all__ = [
     "standard_algorithm",
+    "unfolding_algorithm",
     "n_safe_algorithm",
     "NSafeSettings"
 ]
